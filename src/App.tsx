5c2f8a5671dbfebf896c5a5f52730b7f6b4407f0
WandesonAndrade <wandesonandrade33@gmail.com>
2024-12-26 15:00:30 -0300

    feat: home page end react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { FunctionComponent } from "react";
-import Header from "./components/header/header.component";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/home/home.page";
 
 interface AppProps {
   message?: string;
 }
 
-const App: FunctionComponent<AppProps> = ({ message }) => (
-  <div>
-    <Header />
-  </div>
-);
+const App: FunctionComponent<AppProps> = ({ message }) => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 export default App;
