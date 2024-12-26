8bfce7ab97b4176c5749a36f2b119c7751d5b61d
WandesonAndrade <wandesonandrade33@gmail.com>
2024-12-26 16:11:34 -0300

    feat: add categories type end .env

---
+++ src/components/categories/categories.component.tsx
@@ -0,0 +1,11 @@
+import "./categories.styles.css";
+
+const Categoreis = () => {
+  return (
+    <div className="caregories-container">
+      <div className="categories-content"></div>
+    </div>
+  );
+};
+
+export default Categoreis;
